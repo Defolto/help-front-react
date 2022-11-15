62aea756e9c9b6cb97971645591c2d676cb3cf5a
ma.egorov <defoltmaks@gmail.com>
2022-11-15 17:09:01 +0300

    Добавлена ссылка на телеграм

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,7 @@
 import "./Home.css";
 import AnimateText from "./AnimateText/AnimateText";
+//@ts-ignore
+import img from "./qr_telegram.jpg";
 
 export default function Home(): JSX.Element {
   return (
@@ -14,6 +16,15 @@ export default function Home(): JSX.Element {
         <p>
           Проект, специально разработанный для учеников, которые хотят научиться
           направлению frontend или улучшить свои текущие знания
+        </p>
+      </div>
+      <div className="Home__linkTelegram">
+        <div className="Home__linkTelegramImg">
+          <img src={img} alt="ссылка на телеграм" />
+        </div>
+        <p className="Home__linkTelegramText">
+          Присоединяйся к нам на канал, в котором мы публикуем материалы,
+          статьи, новости и мемы
         </p>
       </div>
     </div>
