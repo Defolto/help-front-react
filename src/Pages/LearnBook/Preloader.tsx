8d51ff6c44edeb09f9de981617dfb00a7cf550db
Anton <antonkhazin2006@gmail.com>
2022-11-22 11:36:54 +0300

    Добавление preloader на страницу учебника

--- src/Pages/LearnBook/Preloader.tsx
+++ src/Pages/LearnBook/Preloader.tsx
@@ -2,11 +2,6 @@ import "./Preloader.css";
 export default function Preloader({}): JSX.Element{
     return <div className="Preloader">
         Загрузка...
-        <div className="load">
-            <hr/>
-            <hr/>
-            <hr/>
-            <hr/>
-        </div>
+        <div className="loads"/>
     </div>
 }
